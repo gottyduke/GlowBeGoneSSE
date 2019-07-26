f65d01d22e9541b1d2fd9e21acfab54347cee479
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-07-25 22:07:05 -0700

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,9 +3,9 @@
 
 bool Settings::loadSettings(bool a_dumpParse)
 {
-	return Json2Settings::Settings::loadSettings(FILE_NAME, false, a_dumpParse);
+	return Json2Settings::Settings::loadSettings(FILE_NAME, a_dumpParse);
 }
 
 
-decltype(Settings::disableForActors)	Settings::disableForActors("disableForActors", true, false);
-decltype(Settings::disableForWeapons)	Settings::disableForWeapons("disableForWeapons", true, false);
+decltype(Settings::disableForActors)	Settings::disableForActors("disableForActors", false);
+decltype(Settings::disableForWeapons)	Settings::disableForWeapons("disableForWeapons", false);
